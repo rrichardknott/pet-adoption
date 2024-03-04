2bc3084c52cc67dae2f70a088560f0da4a8719ac
Richard <richardknott1970@gmail.com>
2024-03-03 21:36:59 -0800

    Working on pet-card section

--- scripts.js
+++ scripts.js
@@ -20,9 +20,31 @@ const petsArea = async () => {
   petsData.forEach((pet) => {
     const clone = template.content.cloneNode(true);
     clone.querySelector("h3").textContent = pet.name;
+    clone.querySelector(".pet-description").textContent = pet.description;
+    clone.querySelector(".pet-birth-year").textContent = getAge(pet.birthYear);
+
+    pet.photo && pet.photo.trim() !== ""
+      ? ((clone.querySelector(".pet-card-photo img").src = pet.photo),
+        (clone.querySelector(
+          ".pet-card-photo img"
+        ).alt = `A ${pet.species} named ${pet.name}`))
+      : ((clone.querySelector(".pet-card-photo img").src =
+          "https://www.robohash.org/animal"),
+        (clone.querySelector(
+          ".pet-card-photo img"
+        ).alt = ` A ${pet.species} named ${pet.name}`));
+
     wrapper.appendChild(clone);
   });
   document.querySelector(".list-of-pets").appendChild(wrapper);
 };
 
 petsArea();
+
+const getAge = (birthYear) => {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - birthYear;
+  if (age == 1) return "1 Year Old";
+  if (age == 0) return "Less than a year old";
+  return `${age} years old`;
+};
